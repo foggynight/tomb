0285bf4d7562befd47dcdae48f21ed0be6280023
foggynight <foggynight709@gmail.com>
2021-01-03 04:43:21 -0330

    Add main function

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * main.cpp - v0.0.0
+ * Copyright (C) 2021 Robert Coffey
+ * Released under the GPLv2 license
+ */
+
+int main()
+{
+    return 0;
+}
+
+/**
+ * Version History
+ * 0.0.0 - 2021-01-03 - First commit
+ */
