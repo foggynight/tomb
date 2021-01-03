7fb2934df4db8ca57557887fec67afde675b8942
foggynight <foggynight709@gmail.com>
2021-01-03 05:20:32 -0330

    Update member comment

--- inc/Tile.hpp
+++ inc/Tile.hpp
@@ -11,7 +11,7 @@
 #define TILE_HPP_
 
 struct Tile {
-    wchar_t symbol;  // Symbol used to represent the Tile on the screen
+    wchar_t symbol;  // Symbol used to represent the Tile on screen
     int colour[2];   // Foreground and background colours of the Tile
     int position[2]; // x-y position of the Tile in its containing Block
 };
