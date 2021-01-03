ca4267152b4f0d5ab13018c013d123d537a59771
foggynight <foggynight709@gmail.com>
2021-01-03 06:02:45 -0330

    Add Block members
    - Add Block members
    - Convert Block from class to struct

--- inc/Block.hpp
+++ inc/Block.hpp
@@ -1,7 +1,7 @@
 /**
  * Block.hpp - v0.0.0
  *
- * Block class representing a block of tiles on the game map.
+ * Block struct representing a square block of tiles on the game map.
  *
  * Copyright (C) 2021 Robert Coffey
  * Released under the GPLv2 license
@@ -10,8 +10,17 @@
 #ifndef BLOCK_HPP_
 #define BLOCK_HPP_
 
-class Block {
+#include <vector>
 
+#include <Tile.hpp>
+
+const int BLOCK_WIDTH = 256;  // Width of each Block's Tile array
+const int BLOCK_HEIGHT = 256; // Height of each Block's Tile array
+
+struct Block {
+public:
+    std::vector<Block*> neighbourVec(8);    // Vector of pointers to neighbouring blocks
+    Tile tileArr[BLOCK_WIDTH*BLOCK_HEIGHT]; // Array of Tiles contained in this block
 };
 
 #endif // BLOCK_HPP_
--- inc/Map.hpp
+++ inc/Map.hpp
@@ -3,7 +3,8 @@
  *
  * Map class representing the game map.
  *
- * The map is made up of blocks, each of which contains an array of tiles.
+ * The map is made up of Blocks, each of which contains a vector of neighbouring
+ * Blocks, and an array of Tiles.
  *
  * Copyright (C) 2021 Robert Coffey
  * Released under the GPLv2 license
