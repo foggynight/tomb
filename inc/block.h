11cd0a5d94d22935fbe36f8cbd2e3f2297d1863e
foggynight <foggynight709@gmail.com>
2021-01-06 23:31:30 -0330

    Refactor block.h

--- inc/block.h
+++ inc/block.h
@@ -1,8 +1,5 @@
 /**
  * block.h - v0.0.0
- *
- * Block type representing a square block of tiles on the game map.
- *
  * Copyright (C) 2021 Robert Coffey
  * Released under the GPLv2 license
  **/
@@ -12,20 +9,39 @@
 
 #include "tile.h"
 
-#define BLOCK_HEIGHT 8 // Height of each block's tile array
-#define BLOCK_WIDTH  8 // Width of each block's tile array
+#define BLOCK_HEIGHT 8 // Height of each block in cells
+#define BLOCK_WIDTH  8 // Width of each block in cells
 
+/**
+ * block_t: Block type containing pointers to neighboring blocks and an
+ *          array of tiles representing tiles in the game world.
+ * @member neighbors Struct of pointers to neighboring blocks
+ * @member tile_arr  Array of tiles, sized: BLOCK_HEIGHT*BLOCK_WIDTH
+ **/
 typedef struct block {
     struct {
-        struct block *above;
-        struct block *right;
-        struct block *below;
-        struct block *left;
-    } neighbors; // struct of pointers to neighboring blocks
-    tile_t tile_arr[BLOCK_HEIGHT*BLOCK_WIDTH]; // Array of tiles contained in this block
+        struct block *above,
+                     *right,
+                     *below,
+                     *left;
+    } neighbors;
+    tile_t tile_arr[BLOCK_HEIGHT*BLOCK_WIDTH];
 } block_t;
 
+/**
+ * block_init: Allocate and initialize a block.
+ * @note Zeroes out the members of the block
+ * @return Pointer to the new block
+ **/
 block_t *block_init(void);
+
+/**
+ * block_get_tile_index: Get the index of the tile at the position:
+ *                       y_pos * BLOCK_WIDTH + x_pos
+ * @param y_pos y position component
+ * @param x_pos x position component
+ * @return Index of the tile at the y-x position
+ **/
 int block_get_tile_index(int y_pos, int x_pos);
 
 #endif // BLOCK_H_
