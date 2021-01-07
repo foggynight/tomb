678754396731459d63719f30e3a957454bad6373
foggynight <foggynight709@gmail.com>
2021-01-07 00:19:18 -0330

    Update tile_t comment

--- inc/tile.h
+++ inc/tile.h
@@ -1,17 +1,18 @@
 /**
  * tile.h - v0.0.0
- *
- * Tile type representing an individual tile on the game map.
- *
  * Copyright (C) 2021 Robert Coffey
  * Released under the GPLv2 license
  **/
 
+/**
+ * tile_t: Tile type representing a tile in the game world.
+ * @member symb Unicode symbol used to represent the tile on screen
+ **/
 #ifndef TILE_H_
 #define TILE_H_
 
 typedef struct tile {
-    const char *symb; // Unicode symbol representing the tile
+    const char *symb;
 } tile_t;
 
 #endif // TILE_H_
