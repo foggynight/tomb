73ae5207e2aad6b28e8b086c0a50369575b1106c
foggynight <foggynight709@gmail.com>
2021-07-17 16:38:29 -0230

    Use structs instead of lists to represent worlds

--- src/world.lisp
+++ src/world.lisp
@@ -1,26 +1,10 @@
 (in-package :tomb)
 
-;;; TODO Use a struct instead of a list to represent worlds
-
-(defun make-world ()
-  "Make a new world."
-  (list nil -1))
-
-(defun get-levels (world)
-  "Get the levels contained within a world."
-  (car world))
-
-(defun set-levels (world levels)
-  "Set the levels contained within a world."
-  (setf (car world) levels))
-
-(defun get-current-level-index (world)
-  "Get the current level index of a world."
-  (cadr world))
-
-(defun set-current-level-index (world index)
-  "Set the current level index of a world."
-  (setf (cadr world) index))
+(defstruct world
+  "World structure representing the game world. Contains a list of levels, as
+well the index of the level currently containing the player."
+  levels                ; List of levels which compose the world
+  current-level-index)  ; Index of the level currently containing the player
 
 (defun generate-world (filenames)
   "Generate a world containing levels which are each constructed using a file
@@ -28,20 +12,20 @@ named in the list of filenames passed as an argument to this function."
   (let ((world (make-world))
         (level-list (loop for filename in filenames
                           collect (read-level-from-file filename))))
-    (set-levels world level-list)
+    (setf (world-levels world) level-list)
     world))
 
-(defun get-level-count (world)
+(defun world-level-count (world)
   "Get the number of levels contained within a world."
-  (length (get-levels world)))
+  (length (world-levels world)))
 
-(defun get-level-n (world n)
+(defun world-level-n (world n)
   "Get the nth level of a world."
-  (car (nthcdr n (get-levels world))))
+  (car (nthcdr n (world-levels world))))
 
-(defun get-current-level (world)
+(defun world-current-level (world)
   "Get the current level of a world."
-  (get-level-n world (cadr world)))
+  (world-level-n world (world-current-level-index world)))
 
 (defun cons-entity (entity level)
   "Cons an entity to the front of the entity list of a level."
@@ -53,28 +37,31 @@ a level."
   (setf (level-entities level) (remove entity (level-entities level) :count n)))
 
 (defun move-to-first-level (world player)
-  "Move a player to the first level contained within a world.
+  "Move a player to the first level contained within a world, returns the new
+current level.
 
 This function does not remove the player from the current level before switching
 levels, thus it should only be called at the start of the program."
-  (set-current-level-index world 0)
-  (cons-entity player (get-current-level world))
-  (get-current-level world))
+  (setf (world-current-level-index world) 0)
+  (cons-entity player (world-current-level world))
+  (world-current-level world))
 
 (defun move-to-next-level (world player)
   "Move a player to the next level contained within a world, wraps around the
 end of the list of worlds, returns the new current level."
-  (remove-entity player (get-current-level world))
-  (set-current-level-index world (mod (1+ (get-current-level-index world))
-                                      (get-level-count world)))
-  (cons-entity player (get-current-level world))
-  (get-current-level world))
+  (remove-entity player (world-current-level world))
+  (setf (world-current-level-index world)
+        (mod (1+ (get-current-level-index world))
+             (get-level-count world)))
+  (cons-entity player (world-current-level world))
+  (world-current-level world))
 
 (defun move-to-previous-level (world player)
   "Move a player to the previous level contained within a world, wraps around
 the beginning of the list of worlds, returns the new current level."
-  (remove-entity player (get-current-level world))
-  (set-current-level-index world (mod (1- (get-current-level-index world))
-                                      (get-level-count world)))
-  (cons-entity player (get-current-level world))
-  (get-current-level world))
+  (remove-entity player (world-current-level world))
+  (setf (world-current-level-index world)
+        (mod (1- (get-current-level-index world))
+             (get-level-count world)))
+  (cons-entity player (world-current-level world))
+  (world-current-level world))
