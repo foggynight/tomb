e36bd081de4a86e6abcbd4bf5b912012d645b1e0
foggynight <foggynight709@gmail.com>
2021-07-19 13:07:31 -0230

    Use triple semicolons for headers only

--- src/tile.lisp
+++ src/tile.lisp
@@ -16,9 +16,9 @@ which determine how the tile interacts with objects in a world."
                          collect (make-tile :symbol char))))
     (make-array (length tile-list) :initial-contents tile-list)))
 
-;;; TODO Remove constraint: strings must be of same length. I think I will go
-;;; about this by padding the ends of shorter strings with spaces to the length
-;;; of the longest string before converting the list of strings to an array.
+;; TODO Remove constraint: strings must be of same length. I think I will go
+;; about this by padding the ends of shorter strings with spaces to the length
+;; of the longest string before converting the list of strings to an array.
 (defun string-list-to-tile-array2 (string-list)
   "Convert a list of strings into a 2D array of tiles, strings must all be the
 same length, returns nil when string-list is empty."
