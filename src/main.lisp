b6dae7a0f4506b1f57d1df984e5ab710a7d7d17a
foggynight <foggynight709@gmail.com>
2021-07-22 15:32:51 -0230

    Add initial player position and update test levels

--- src/main.lisp
+++ src/main.lisp
@@ -2,10 +2,8 @@
 
 (defun main ()
   (let* ((world (generate-world '("res/test-0.lvl"
-                                  "res/test-1.lvl"
-                                  "res/test-2.lvl"
-                                  "res/test-3.lvl")))
-         (player (make-player))
+                                  "res/test-1.lvl")))
+         (player (make-player :y 1 :x 1))
          (level (move-to-first-level world player)))
     (crt:with-screen (scr :cursor-visible nil
                           :input-echoing nil
