a32d2aa9d7840b4f5fca9274f7563ed2ecbfc22f
foggynight <foggynight709@gmail.com>
2021-07-22 23:51:41 -0230

    Add more windows
    - Renamed draw-level to draw-game-view
    - Renamed win variable and draw-game-view win parameter to game-window
    - Added the note, stat, and depth windows
    - Added draw-initial-ui and draw-depth-indicator-label
    - Added a call to draw-initial-ui after binding events in main

--- src/main.lisp
+++ src/main.lisp
@@ -8,26 +8,45 @@
     (crt:with-screen (scr :cursor-visible nil
                           :input-echoing nil
                           :process-control-chars nil)
-      (crt:with-window (win :dimensions '(24 80)
-                            :position '(0 0))
-        (crt:bind win #\ 'crt:exit-event-loop)
+      (crt:with-windows ((note-window
+                          :background (make-instance 'crt:complex-char
+                                                     :simple-char #\N)
+                          :dimensions '(1 80)
+                          :position '(0 0))
+                         (game-window
+                          :dimensions '(22 80)
+                          :position '(1 0))
+                         (stat-window
+                          :background (make-instance 'crt:complex-char
+                                                     :simple-char #\S)
+                          :dimensions '(1 71)
+                          :position '(23 0))
+                         (depth-window
+                          :background (make-instance 'crt:complex-char
+                                                     :simple-char #\D)
+                          :dimensions '(1 8)
+                          :position '(23 72)))
+        (crt:bind game-window #\ 'crt:exit-event-loop)
         (macrolet ((bind (key direction)
-                     `(crt:bind win ,key
+                     `(crt:bind game-window ,key
                                 (lambda (w e)
                                   (declare (ignore w e))
                                   (attempt-move player level ,direction)
-                                  (draw-level win level)))))
+                                  (draw-game-view game-window level)))))
           (bind #\h :left)
           (bind #\j :down)
           (bind #\k :up)
           (bind #\l :right))
-        (crt:bind win #\>
+        (crt:bind game-window #\>
                   (lambda (w e)
                     (declare (ignore w e))
                     (when (level-tile-is-stairs-p level
                                                   (entity-y player)
                                                   (entity-x player))
                       (setq level (move-to-next-level world player))
-                      (draw-level win level))))
-        (draw-level win level)
-        (crt:run-event-loop win)))))
+                      (draw-game-view game-window level))))
+        (draw-initial-ui note-window
+                         stat-window
+                         depth-window)
+        (draw-game-view game-window level)
+        (crt:run-event-loop game-window)))))
--- src/screen.lisp
+++ src/screen.lisp
@@ -1,20 +1,27 @@
 (in-package :tomb)
 
-(defun draw-level (win level)
-  (crt:clear win)
+(defun draw-depth-indicator-label (depth-window)
+  (crt:add depth-window "Depth:" :y 0 :x 0))
+
+(defun draw-initial-ui (note-window stat-window depth-window)
+  (draw-depth-indicator-label depth-window)
+  (mapcar #'crt:refresh (list note-window stat-window depth-window)))
+
+(defun draw-game-view (game-window level)
+  (crt:clear game-window)
 
   ;; Draw tiles
   (let* ((tiles (level-tiles level))
          (row-width (array-dimension tiles 1)))
     (dotimes (row-index (array-dimension tiles 0))
       (dotimes (cell-index row-width)
-        (crt:add win (tile-symbol (aref tiles row-index cell-index)))
-        (crt:move win 1 (- row-width) :relative t))))
+        (crt:add game-window (tile-symbol (aref tiles row-index cell-index)))
+        (crt:move game-window 1 (- row-width) :relative t))))
 
   ;; Draw entities
   (let ((entities (level-entities level)))
     (dolist (entity entities)
-      (crt:add win (entity-symbol entity) :y (entity-y entity)
-                                          :x (entity-x entity))))
+      (crt:add game-window (entity-symbol entity) :y (entity-y entity)
+                                                  :x (entity-x entity))))
 
-  (crt:refresh win))
+  (crt:refresh game-window))
